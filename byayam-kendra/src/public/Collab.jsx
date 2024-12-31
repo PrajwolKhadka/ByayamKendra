d5da6580d7bd3d4bcf388949c3b3204ab2c1beb0
PrajwolKhadka <prazolkhadka67@gmail.com>
2024-12-31 07:36:08 +0545

    Corrected the image path

--- byayam-kendra/src/public/Collab.jsx
+++ byayam-kendra/src/public/Collab.jsx
@@ -2,29 +2,28 @@ import React from 'react'
 import '../css//Landingpage.css'
 function Collab(){
     const brands = [
-        { logo: "../assests/brand1.jpg" },
-        { logo: "../assests/brand2.png" },
-        {logo: "../assests/brand3.png" },
-        { logo: "../assests/brand4.png" },
-        { logo: "../assests/brand5.jpg" },
-        { logo: "../assests/brand6.png" },
-        { logo: "../assests/brand7.png" },
-        { logo: "../assests/brand8.jpg" },
-        { logo: "../assests/brand9.png" },
-        { logo: "../assests/brand10.png" },
-        { logo: "../assests/brand11.png" },
-        { logo: "../assests/brand12.jpg" },
-        { logo: "../assests/brand13.png" },
-        { logo: "../assests/brand14.jpg" },
-        { logo: "../assests/brand15.png" },
-        { logo: "../assests/brand16.jpg" },
-        { logo: "../assests/brand17.jpg" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        {logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
+        { logo: "/resources/collab1.png" },
       ];
     return(
         <section className='ScrollView' id="#collab">
         <div className="Collab" id="plans">
-            <h1 className='Collabh1'>Our Partners</h1>
-            <p>साझेदारी र समजदारी</p>
+            <h1 className='Collabh1'>We Are Stronger With</h1>
         </div>
     <div className="horizontal-scroll-container">
      <div className="horizontal-scroll">
