37ead524757375d2c0f120d830da3480b218d3d9
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-02-06 19:20:08 +0545

    Updated userModel.js

--- byayam-backend/model/userModel.js
+++ byayam-backend/model/userModel.js
@@ -1,9 +1,9 @@
 import { pool } from '../db/db.js';
 
-export const createUser = async (username, email, password, gender) => {
+export const createUser = async (username, email, password, gender, role) => {
   try {
-    const query = 'INSERT INTO users (username, email, password, gender) VALUES ($1, $2, $3, $4) RETURNING *';
-    const values = [username, email, password, gender];
+    const query = 'INSERT INTO users (username, email, password, gender, role) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+    const values = [username, email, password, gender, role];
     const result = await pool.query(query, values);
     return result.rows[0];
   } catch (error) {
@@ -11,7 +11,6 @@ export const createUser = async (username, email, password, gender) => {
     throw new Error(process.env.NODE_ENV === 'production' ? 'Database error during user creation' : error.message);
   }
 };
-
 
 export const findUserByEmailOrUsername = async (email, username) => {
   try {
@@ -25,12 +24,11 @@ export const findUserByEmailOrUsername = async (email, username) => {
   }
 };
 
-
 export const findUserByEmail = async (email) => {
   const query = `
     SELECT * FROM users 
     WHERE email = $1;
   `;
   const { rows } = await pool.query(query, [email]);
-  return rows[0];
+  return rows[0]; // Ensure this returns the user object including the role
 };
